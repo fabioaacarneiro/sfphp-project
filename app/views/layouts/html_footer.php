008931eca43b85de73bec2405d8e4cc9a90867a8
Fabio Carneiro <fabioaacarneiro@gmail.com>
2023-09-18 22:23:22 -0300

    criado templates header e footer base

---
+++ app/views/layouts/html_footer.php
@@ -0,0 +1,6 @@
+<!-- bootstrap -->
+<script src="assts/bootstrap/bootstrap.bundle.min.js"></script>
+<!-- main app javascript -->
+<script src="assts/app.js"></script>
+</body>
+</html>
---
+++ app/views/layouts/html_header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= APP_NAME ?></title>
+    <!-- font google -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Titillium+Web:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700&display=swap" rel="stylesheet">
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="assets/bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/fontawesome/all.min.css">
+    <!-- main app.css file -->
+    <link rel="stylesheet" href="assets/app.css">
+</head>
+
+<body>
