feae34636e9afed113f31aecd4403fe50c509f18
Fabio Carneiro <fabioaacarneiro@gmail.com>
2023-09-19 08:28:56 -0300

    BaseModel criado

--- app/config.php
+++ app/config.php
@@ -1,3 +1,9 @@
 <?php
 
 define("APP_NAME", "Basic Name Gathering");
+
+// database
+define("MYSQL_HOST", "localhost");
+define("MYSQL_DATABASE", "db_bng");
+define("MYSQL_USER", "root");
+define("MYSQL_PASSWORD", "");
---
+++ app/models/BaseModel.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace bgn\Models;
+
+use bng\System\Database;
+
+abstract class BaseModel
+{
+    public $db;
+
+    public function db_connect()
+    {
+        $options = [
+            "host" => MYSQL_HOST,
+            "database" => MYSQL_DATABASE,
+            "username" => MYSQL_USER,
+            "password" => MYSQL_PASSWORD
+        ];
+
+        $this->db = new Database($options);
+    }
+
+    public function query($sql = "", $params = [])
+    {
+        return $this->db->execute_query($sql);
+    }
+}
