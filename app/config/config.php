be25f3fefa1c140bd85c8c2e416a579d48cb339d
Fabio Carneiro <fabioaacarneiro@gmail.com>
2023-10-15 09:45:58 -0300

    Updated config of project to works with .env or not in case to send for production and remove .env file

---
+++ app/config/config.php
@@ -0,0 +1,29 @@
+<?php
+
+$settings = require_once __DIR__ . "/settings.php";
+
+define("APP_NAME", "Simple Framework PHP");
+
+if (file_exists($dotenv = Dotenv\Dotenv::createImmutable(__DIR__))) {
+
+    echo "<br>Arquivo existe<br>";
+    $dotenv->load();
+    
+    // database setup with .env if exists
+    define("MYSQL_HOST", $_ENV["MYSQL_HOST"]);
+    define("MYSQL_DATABASE", $_ENV["MYSQL_DATABASE"]);
+    define("MYSQL_USER", $_ENV["MYSQL_USER"]);
+    define("MYSQL_PASSWORD", $_ENV["MYSQL_PASSWORD"]);
+    
+    echo "<pre>";
+    var_dump($dotenv);
+
+} else {
+    $database = $settings["database"];
+
+    // database setup with .env not if exists
+    define("MYSQL_HOST", $database["MYSQL_HOST"]);
+    define("MYSQL_DATABASE", $database["MYSQL_DATABASE"]);
+    define("MYSQL_USER", $database["MYSQL_USER"]);
+    define("MYSQL_PASSWORD", $database["MYSQL_PASSWORD"]);
+}
---
+++ app/config/settings.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    "database" => [
+        "MYSQL_HOST" => "your_host_name",
+        "MYSQL_DATABASE" => "your_database_name",
+        "MYSQL_USER" => "your_user_name",
+        "MYSQL_PASSWORD" => "your_password",
+    ],
+];
